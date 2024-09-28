36ac0d6eb5ed265fdc205ed8aadda738db7ea752
G M Ganesh <ganesh.gm@magnificenztech.com>
2024-09-28 23:51:07 +0530

    first commit

---
+++ src/App.jsx
@@ -0,0 +1,14 @@
+import { useState } from 'react'
+
+function App() {
+
+  return (
+    <>
+      <div className='h-full w-full bg-red-400'>
+        Hello World
+        </div>
+    </>
+  )
+}
+
+export default App
